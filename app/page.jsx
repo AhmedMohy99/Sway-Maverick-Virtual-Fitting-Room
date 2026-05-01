ce784a8a34d39bd5b4161f2d376e1c355925f5cf
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-05-01 18:38:59 +0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+import SwayFittingRoom from '../components/SwayFittingRoom';
+
+export default function Home() {
+  return (
+    <main style={{ width: '100vw', height: '100vh' }}>
+      <SwayFittingRoom />
+    </main>
+  );
+}
