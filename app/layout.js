f73051431cca5cd3cd69318a39bcafa100a2f5d2
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-05-01 19:34:13 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,7 @@
-export const metadata = {
-  title: 'Sway Maverick | Virtual Fitting Room',
-  description: 'Powered by We-Wave-Agency',
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, padding: 0, backgroundColor: '#050505' }}>
-        {children}
-      </body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   )
 }
