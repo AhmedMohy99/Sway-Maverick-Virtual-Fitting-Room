3d690e8549df60c4c8972f1b29306bc544d0022f
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-05-03 13:41:54 +0300

    Update store.ts

--- lib/store.ts
+++ lib/store.ts
@@ -1,23 +1,44 @@
 import { create } from 'zustand';
 
 interface SwayState {
+  // بيانات الجسم (التي يطلبها page.tsx)
+  height: number;
+  weight: number;
+  recommendedSize: string;
+  isSizeGuideOpen: boolean;
+  
+  // بيانات الملابس (الـ Mix & Match)
   topTexture: string | null;
   bottomTexture: string | null;
   faceTexture: string | null;
   topFit: string;
   bottomFit: string;
-  // أضف الدوال لتحديث كل قطعة بشكل منفصل
+
+  // الدوال (Actions)
+  setBodyData: (h: number, w: number) => void;
+  updateRecommendation: (size: string) => void;
+  toggleSizeGuide: (isOpen: boolean) => void;
   setTop: (url: string, fit: string) => void;
   setBottom: (url: string, fit: string) => void;
   setFace: (url: string) => void;
 }
 
 export const useSwayStore = create<SwayState>((set) => ({
+  // القيم الابتدائية
+  height: 170,
+  weight: 70,
+  recommendedSize: 'Calculating...',
+  isSizeGuideOpen: false,
   topTexture: null,
   bottomTexture: null,
   faceTexture: null,
   topFit: 'oversized',
   bottomFit: 'pants',
+
+  // تنفيذ الدوال
+  setBodyData: (h, w) => set({ height: h, weight: w }),
+  updateRecommendation: (size) => set({ recommendedSize: size }),
+  toggleSizeGuide: (isOpen) => set({ isSizeGuideOpen: isOpen }),
   setTop: (url, fit) => set({ topTexture: url, topFit: fit }),
   setBottom: (url, fit) => set({ bottomTexture: url, bottomFit: fit }),
   setFace: (url) => set({ faceTexture: url }),
